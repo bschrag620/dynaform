6487fa60a5a1683ad25b656ce9ed88aa04e9a515
Brad Schrag <bschrag@vmox.com>
2021-09-12 01:17:12 -0500

    Styling: Add bootstrap and initial styling

--- app/frontend/packs/application.js
+++ app/frontend/packs/application.js
@@ -8,6 +8,7 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 require("controllers")
+import '../js/bootstrap_js_files'
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
--- app/models/dyna_form.rb
+++ app/models/dyna_form.rb
@@ -6,7 +6,7 @@ class DynaForm < ApplicationRecord
 
   has_many :form_inputs
 
-  after_create_commit { broadcast_append_to "dyna_forms" }
+  after_create_commit { broadcast_prepend_to "dyna_forms" }
   after_update_commit { broadcast_replace_to "dyna_forms" }
   after_destroy_commit { broadcast_remove_to "dyna_forms" }
 end
