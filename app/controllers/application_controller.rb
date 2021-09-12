a0cbaf7375ce76f768ff321e65d968cd17937f68
Brad Schrag <bschrag@vmox.com>
2021-09-12 01:51:45 -0500

    Logout: Fix bug keeping user from logging out
    * deletes session cookie on logout
    * refactors app to force login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  before_action :set_current
+  before_action :set_current, :redirect_if_not_logged_in
 
   def index
     @dyna_forms = DynaForm.all
@@ -9,5 +9,9 @@ class ApplicationController < ActionController::Base
   private
   def set_current
     Current.session = Session.find_by(id: session[:current_session_id]) if session.include?(:current_session_id)
+  end
+
+  def redirect_if_not_logged_in
+    redirect_to login_url unless Current.user
   end
 end
